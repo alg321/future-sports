e9d446405c9e715dd01e5731b5d4d57d910635bd
Your Name <you@example.com>
2023-12-12 00:23:23 +1100

    sixth commit

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path='/App' element={<App />} />
+      <Route path='/' element={<App />} />
       <Route path='/ClassTimetable' element={<ClassTimetable />} />
       <Route path='/Classes' element={<Classes />} />
       <Route path='/JoinNow' element={<JoinNow />} />
